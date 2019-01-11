58dcf57948ae7c8209d8e68534a69b310e8b7479
Phoebe <pcehb@icloud.com>
2019-01-11 18:36:16 +0000

    Styling

--- src/app/charts/charts.page.ts
+++ src/app/charts/charts.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RssProvider } from '../providers/rss/rss';
 import { HttpClient} from '@angular/common/http';
-import { iTunesDbService} from '../services/itunes-db.service';
 
 @Component({
   selector: 'app-chart',
@@ -17,7 +16,7 @@ export class ChartPage implements OnInit {
   loader: any;
   url: any = "https://rss.itunes.apple.com/api/v1/gb/podcasts/top-podcasts/all/100/explicit.rss";
 
-  constructor( public http: HttpClient, private api: iTunesDbService, public rssProvider: RssProvider) {
+  constructor( public http: HttpClient, public rssProvider: RssProvider) {
   }
 
   Get_Chart_RSS_Data(feedUrl) {
@@ -29,19 +28,14 @@ export class ChartPage implements OnInit {
    );
  }
 
-//  Get_RSS_Data(feedUrl) {
-//    this.rssProvider.GetRSS(feedUrl, "desc").subscribe(
-//        data => {
-//          this.rssDataArrayDesc = data;
-//        }
-//    );
-// }
+getTitle(title){
+  var array = title.split("-");
+  return array[0];
+}
 
-getDate(pubDate){
-  var date = new Date(pubDate);
-  var months = Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-  var string = date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear()
-  return string;
+getName(title){
+  var array = title.split("-");
+  return array[1];
 }
 
 detailsPage(link){
